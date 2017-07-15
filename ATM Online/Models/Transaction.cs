553df162ff2df22f076938e5e50204eb835829e9
ebmacias45 <ebmacias45@gmail.com>
2017-07-15 16:22:48 -0400

    Add All Files

---
+++ ATM Online/Models/Transaction.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Web;
+
+namespace ATM_Online.Models
+{
+    public class Transaction
+    { 
+        public Transaction()
+        {
+            Date = DateTime.Now;
+        }
+
+        public int Id { get; set;}
+
+        [Required]
+        [DataType(DataType.Currency)]
+        public decimal Amount { get; set; }
+
+        [Required]
+        public int CheckingAccountId { get; set; }
+        public virtual ChekingAccount CheckingAccount { get; set; }
+
+        
+        public DateTime Date { get; private set; }
+    }
+}
